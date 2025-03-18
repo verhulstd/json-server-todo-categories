0426c1c91a47451a87273b8619ee9056324a6676
David Verhulst <info@inventivemedia.be>
2025-03-18 23:25:58 +0100

    update

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const jsonServer = require("json-server");
 const multer = require("multer");
+const express = require("express");
 const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
+server.use("/upload", express.static(path.join(__dirname, "upload")));
 // Set up multer for image upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
