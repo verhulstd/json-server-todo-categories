84016a2cb8a8a0cbdd40f739a621d4ce34bc5eed
David Verhulst <info@inventivemedia.be>
2025-03-18 22:42:56 +0100

    added upload

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
+const multer = require("multer");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3200;
+
+// Set up multer for image upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "upload");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1 * 1024 * 1024 },
+}); // Limit to 1MB
+
+// Custom route for image upload
+server.post("/upload", upload.array("image", 12), (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No file uploaded or invalid file type.");
+  }
+  res.send("Image(s) uploaded successfully.");
+});
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
