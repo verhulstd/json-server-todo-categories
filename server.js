45d7b9307d9cc5adca27382f86f514b8053efcf7
David Verhulst <info@inventivemedia.be>
2025-03-18 22:59:53 +0100

    added upload

--- server.js
+++ server.js
@@ -7,21 +7,18 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
 // Set up multer for image upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "upload");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1 * 1024 * 1024 },
-}); // Limit to 1MB
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "upload");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   },
+// });
+const upload = multer({ dest: "./upload/" });
 
 // Custom route for image upload
-server.post("/upload", upload.array("image", 12), (req, res) => {
+server.post("/upload", upload.single("image"), (req, res) => {
   if (!req.files) {
     return res.status(400).send("No file uploaded or invalid file type.");
   }
